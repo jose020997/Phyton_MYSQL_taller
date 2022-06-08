5c9601cee1654f6cfaeea2dc2b844129e8159fd1
Jose Antonio Barrios García <joaebasket13@gmail.com>
2022-06-08 18:55:54 +0200

    menu == 5

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ while not menus:
         print("1. Ingresar un Cliente")
         print("2. Nuevo vehiculo")
         print("3. Dar de alta a un Mecanico")
-        print("4. Consultas")
-        print("5. Abrir una averia")
-        print("6. Modificaciones") #Salario de un mecanico, dueño de un coche 
-        print("7. Salir")
+        print("4. Abrir una averia")
+        print("5. Consultas")
+        print("6. Modificaciones") #Salario de un mecanico, en arregla cambiar fec_rep y horas_empleadas
+        print("7. Dar de baja un mecanico")
+        print("8. Salir")
         print("")
         menu=int(input("---> "))
         if menu == 1:
@@ -112,19 +113,75 @@ while not menus:
             correcto=input("¿Estan los datos correctos?  ")
             if correcto == "si" or correcto == "s":
                 if a.Consulta_Condicion("*","mecanico","DNI="+dni_t) != []:
-                    print("Ya existe este mecanico artista")
+                    print("Ya existe este mecanico")
                 else:
                     a.Insertar_M(dni_t,Nombre,fecha_t,salario_t)    
                     print("Mecanico insertado correctamente")
             else:
                 print("anulando operacion")    
         elif menu == 4:
-            print()
-            
+            print("Buenas tardes, vamos a gestionar las averias")
+            cod_averia=len(a.Consulta("*","arregla"))+1 #Autoincrementamos en 1
+            mat=input("Introduce la matricula ")
+            b=a.Consulta("Nombre,DNI","mecanico")
+            for x in range(0,len(b)):
+                print(b[x])
+            DNIm=int(input("Introduce el DNI del mecanico: "))
+            Fecha="0000/00/00" #fecha actual en el formato correcto
+            horas="0"
+            cod_t=str(cod_averia)
+            dni_t=str(DNIm)
+            problema=input("Introduce el problema ")
+            if a.Consulta_Condicion("*","coche","Matricula="+"'"+mat+"'") != []:
+                print("Ese vehiculo existe")
+                if a.Consulta_Condicion("*","mecanico","DNI="+dni_t) != []:
+                    print("existe todo")
+                    a.Insertar_A(cod_t,mat,dni_t,Fecha,horas,problema)
+                    print("Averia correcta")
+                else:
+                    print("No existe ese mecanico")    
+            else:
+                print("Ese vehiculo no existe, tendria que darlo de alta") #Otra opcion copiar todo de menu == 2 y asi desde aqui generas el vehiculo y luego das de alta todo
         elif menu == 5:
-            print()
+            menu5=False
+            while not menu5:
+                print("")
+                print("1. Consultar Averias pendientes")
+                print("2. Cantidad de coches de un cliente")
+                print("3. Salir")
+                print("")
+                menu5_op=int(input("---> "))
+                if menu5_op==1:
+                    averi=len(a.Consulta_Condicion("*","arregla","Horas=0"))
+                    print("Tenemos un total de "+str(averi)+" sin resolver que son :")
+                    averi=a.Consulta_Condicion("Cod_Averia,Matricula,DNI,Problema","arregla","Horas=0")
+                    print("")
+                    print("Cod,Matricula,Mecanico,Problema")
+                    for x in range(0,len(averi)):
+                        print(averi[x])
+
+                elif menu5_op==2:
+                    dni=input("Introduce el DNI del cliente ")
+                    averi=str(len(a.Consulta_Condicion("matricula,modelo,marca","coche","DNI="+dni)))
+                    coches=a.Consulta_Condicion("matricula,modelo,marca","coche","DNI="+dni)
+                    nombre=str(a.Consulta_Condicion("nombre","cliente","DNI="+dni))
+                    characters = "[](),'" #caracteres que vamos a quitar
+                    for x in range(len(characters)): #recorremos todos los caracteres y quitamos lo que sobra
+                        nombre = nombre.replace(characters[x],"")
+                    print("")    
+                    print(nombre +" tiene un total de: "+averi+" coches")
+                    print("")
+                    print("matricula ,modelo, marca")
+                    print("")
+                    for x in range(0,len(coches)):
+                        print(coches[x])
+
+                else:        
+                    menu5=True
         elif menu == 6:
-            print()    
+            print()   
+        elif menu== 7:
+            print()     
         else:
             menus=True
             
